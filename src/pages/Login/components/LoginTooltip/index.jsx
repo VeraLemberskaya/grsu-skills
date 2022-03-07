ed6847161076ae207ebfc7201040a5a9ea8e6ed0
Vera Lemberskaya <veralemberskaa@gmail.com>
2022-03-07 22:31:15 +0300

    added Tooltip

---
+++ src/pages/Login/components/LoginTooltip/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Tooltip = ({ visibility }) => {
+  return (
+    <div className={`login-tooltip-wrapper ${visibility ? "visible" : ""}`}>
+      <div className="login-tooltip">
+        <div className="tooltip-text">
+          <p>
+            Данные своего личного аккаунта для входа в систему grsu.skills
+            выдаются после поступления в университет.
+          </p>
+        </div>
+        <div className="line"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Tooltip;
