33272c4bd636874b64fe9a18eca716adeace6449
Vera Lemberskaya <veralemberskaa@gmail.com>
2022-04-03 16:51:17 +0300

    added competencies requests

--- src/api/ApiRequests/index.js
+++ src/api/ApiRequests/index.js
@@ -1,8 +1,24 @@
 import API from "../../api";
-import { allFacultiesEndPoint } from "../../constants";
+import {
+  allFacultiesEndPoint,
+  allCompetenciesEndPoint,
+  competenciesEndPoint,
+} from "../../constants";
 
 export const getFaculties = async () => {
   return await API.get(allFacultiesEndPoint)
+    .then((response) => response.data)
+    .catch((error) => console.log(error));
+};
+
+export const getCompetencies = async () => {
+  return await API.get(allCompetenciesEndPoint)
+    .then((response) => response.data)
+    .catch((error) => console.log(error));
+};
+
+export const getCompetenciesByLetter = async (letter) => {
+  return await API.post(competenciesEndPoint, null, { params: letter })
     .then((response) => response.data)
     .catch((error) => console.log(error));
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
 export const grsuSkillsURL = "https://localhost:7042";
 
 export const allFacultiesEndPoint = "api/Faculties/faculties";
+export const allCompetenciesEndPoint = "api/Competencies/compentencies";
+export const competenciesEndPoint = "api/Competencies/compantencies";
